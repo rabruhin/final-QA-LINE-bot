da10d2f244bcbf00c21e2655c4233fde6525d4b9
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-09-19 12:41:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(f"關於此訊息，找到的消息為:")
+    try:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(f"關於此訊息，找到的消息為:")
 
     # 先回應新 QA 系統的回答
     try:
